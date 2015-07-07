056c39d2bd85352a827aaa5beafdc8496ec6b3e7
Tyler Breland <workwith@tylerbreland.me>
2015-07-06 21:02:47 -0500

    better install scripts

--- scripts/install_pcars.sh
+++ scripts/install_pcars.sh
@@ -3,8 +3,10 @@ BASE_DIR="$( cd $SCRIPT_DIR && cd .. && pwd)"
 STEAMCMD_DIR="$BASE_DIR/steamcmd"
 export CPU_MHZ=3000 # fix for some virtualmachines
 
-"$STEAMCMD_DIR/steamcmd.sh" +login anonymous +force_install_dir "$STEAMCMD_DIR/pcars_ds" +app_update 332670 validate +exit
-
-if [[ "$UNAMESTR" == 'Linux' ]]; then
+if [ ! -f "$STEAMCMD_DIR/pcars_ds/DedicatedServerCmd" ]; then
+  "$STEAMCMD_DIR/steamcmd.sh" +login anonymous +force_install_dir "$STEAMCMD_DIR/pcars_ds" +app_update 332670 validate +exit
   cp -R "$STEAMCMD_DIR/linux32/"* "$STEAMCMD_DIR/pcars_ds/lib32/"
+  echo "successfully installed pcars dedicated server"
+else
+  echo "pcars dedicated server is already installed"
 fi
--- scripts/install_steamcmd.sh
+++ scripts/install_steamcmd.sh
@@ -3,16 +3,13 @@ BASE_DIR="$( cd $SCRIPT_DIR && cd .. && pwd)"
 STEAMCMD_DIR="$BASE_DIR/steamcmd"
 UNAMESTR=`uname`
 
-mkdir -p $STEAMCMD_DIR
-
-if [[ "$UNAMESTR" == 'Linux' ]]; then
+if [ ! -f "$STEAMCMD_DIR/steamcmd.sh" ]; then
   wget "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" -O "$STEAMCMD_DIR/steamcmd.tar.gz"
-elif [[ "$UNAMESTR" == 'Darwin' ]]; then # os x
-  curl -o "$STEAMCMD_DIR/steamcmd.tar.gz" "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_osx.tar.gz"
+  tar -zxvf "$STEAMCMD_DIR/steamcmd.tar.gz" -C $STEAMCMD_DIR
+  rm "$STEAMCMD_DIR/steamcmd.tar.gz"
+  chmod +x "$STEAMCMD_DIR/steamcmd.sh"
+  chmod +x "$STEAMCMD_DIR/steam.sh"
+  echo "successfully installed steamcmd"
+else
+  echo "steamcmd is already installed"
 fi
-
-
-tar -zxvf "$STEAMCMD_DIR/steamcmd.tar.gz" -C $STEAMCMD_DIR
-rm "$STEAMCMD_DIR/steamcmd.tar.gz"
-chmod +x "$STEAMCMD_DIR/steamcmd.sh"
-# chmod +x "$STEAMCMD_DIR/steam.sh"
