f822cf29ce681cb0187236868553c5afaaedbfa7
Tyler Breland <workwith@tylerbreland.me>
2015-07-08 17:00:07 -0500

    cleaned up admin controller

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -6,41 +6,41 @@
  */
 
 
-module.exports = {
+ module.exports = {
 
-    adminConnect: function(req, res) {
-        var roomName = "Admin";
-        sails.sockets.join(req.socket, roomName);
-        console.log("connected to :"+roomName);
-    },
+  adminConnect: function(req, res) {
+    var roomName = "Admin";
+    sails.sockets.join(req.socket, roomName);
+    console.log("connected to :"+roomName);
+  },
 
-    dashboard: function (req, res) {
-        res.locals.layout = 'Admin/layout';
-        async.series({
-            drivers: function (callback) {
-                Driver.find().exec(function(error, found) {
-                    callback(null, found);
-                });
-            },
-            laps : function (callback) {
-                Lap.find().exec(function(error, found){
-                    callback(null, found);
-                });
-            },
-            sessions: function (callback) {
-                ServerSession.find().exec(function(error, found){
-                    callback(null, found);
-                });
-            }
-        }, function (err, results) {
-
-            return res.view('Admin/dashboard',{
-                drivers: results.drivers,
-                laps: results.laps,
-                sessions: results.sessions
-            });
+  dashboard: function (req, res) {
+    res.locals.layout = 'Admin/layout';
+    async.series({
+      drivers: function (callback) {
+        Driver.find().exec(function(error, found) {
+          callback(null, found);
         });
-    },
+      },
+      laps : function (callback) {
+        Lap.find().exec(function(error, found){
+          callback(null, found);
+        });
+      },
+      sessions: function (callback) {
+        ServerSession.find().exec(function(error, found){
+          callback(null, found);
+        });
+      }
+    }, function (err, results) {
+
+      return res.view('Admin/dashboard',{
+        drivers: results.drivers,
+        laps: results.laps,
+        sessions: results.sessions
+      });
+    });
+  },
 
 
 };
