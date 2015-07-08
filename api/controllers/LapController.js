ec627b4c58f47c55273485176f33bad62a52f37a
Tyler Breland <workwith@tylerbreland.me>
2015-07-08 16:47:35 -0500

    dry'd up lapscontroller

--- api/controllers/LapController.js
+++ api/controllers/LapController.js
@@ -6,50 +6,35 @@
  */
 
 
-module.exports = {
+ module.exports = {
 
-    adminIndex : function(req, res) {
-      res.locals.layout = 'Admin/layout';
-        if (req.param('page')) {
-            Lap.count(function(err, count){
-                Lap.find().sort("createdAt DESC").paginate({page: req.param('page'), limit: sails.config.personnalConfig.pagination.laps.admin.limit}).populateAll().exec(function (err, laps){
-                    return res.view('Admin/Lap/index',{
-                        laps: laps,
-                        pagination: {
-                            page: req.param('page'),
-                            href: '/admin/laps/',
-                            count: Math.round((count / sails.config.personnalConfig.pagination.laps.admin.limit))
-                        }
-                    });
-                });
-            });
-        } else {
-            Lap.count(function(err, count){
-                Lap.find().sort("createdAt DESC").paginate({page: 1, limit: sails.config.personnalConfig.pagination.laps.admin.limit}).populateAll().exec(function (err, laps){
-                    return res.view('Admin/Lap/index',{
-                        laps: laps,
-                        pagination: {
-                            page: 1,
-                            href: '/admin/laps/',
-                            count: Math.round((count / sails.config.personnalConfig.pagination.laps.admin.limit))
-                        }
+  adminIndex : function(req, res) {
+    res.locals.layout = 'Admin/layout';
+    Lap.count(function(err, count){
+      Lap.find().sort("createdAt DESC").paginate({
+        page: req.param('page') || 1,
+        limit: sails.config.personnalConfig.pagination.laps.admin.limit
+      }).populateAll().exec(function (err, laps){
+        return res.view('Admin/Lap/index',{
+          laps: laps,
+          pagination: {
+            page: req.param('page'),
+            href: '/admin/laps/',
+            count: Math.round((count / sails.config.personnalConfig.pagination.laps.admin.limit))
+          }
+        });
+      });
+    });
+  },
 
-                    });
-                });
-            });
-
-        }
-    },
-
-    delete: function (req, res) {
-        if (req.param('id')) {
-            Lap.findOne(req.param('id')).exec(function (err, lap){
-                if (err) return res.json(500, {msg: "Unable to find the lap in Database", class: 'alert-danger'});
-                Lap.destroy(lap.id).exec(function(err){
-                    if (err) return res.json(500, {msg: err});
-                    return res.json(200, {msg: "Lap deleted", class: 'alert-success'});
-                });
-            });
-        }
-    }
+  delete: function (req, res, next()) {
+    if (!req.param('id')) return next();
+    Lap.findOne(req.param('id')).exec(function (err, lap){
+      if (err) return res.json(500, {msg: "Unable to find the lap in Database", class: 'alert-danger'});
+      Lap.destroy(lap.id).exec(function(err){
+        if (err) return res.json(500, {msg: err});
+        return res.json(200, {msg: "Lap deleted", class: 'alert-success'});
+      });
+    });
+  }
 };
