a6cbe17544f91b7ed0aa973d565a758576a620e5
Tyler Breland <workwith@tylerbreland.me>
2015-07-08 16:05:31 -0500

    dry'd up the cars controller

--- api/controllers/CarController.js
+++ api/controllers/CarController.js
@@ -6,90 +6,71 @@
  */
 
 
-module.exports = {
+ module.exports = {
 
-    index: function (req, res) {
-        res.locals.layout = 'Admin/layout';
-        if (req.param('page')) {
-            Car.count(function(err, count){
-                Car.find().sort('name ASC').paginate({page: req.param('page'), limit: sails.config.personnalConfig.pagination.cars.admin.limit}).exec(function (err, cars){
-                    return res.view('Admin/Car/index',{
-                        cars: cars,
-                        pagination: {
-                            page: req.param('page'),
-                            href:'/admin/cars/',
-                            count: Math.round((count / sails.config.personnalConfig.pagination.cars.admin.limit))
-                        }
-                    });
-                });
-            });
-        } else {
-            Car.count(function(err, count){
-                Car.find().sort('name ASC').paginate({page: 1, limit: sails.config.personnalConfig.pagination.cars.admin.limit}).exec(function (err, cars){
-                    return res.view('Admin/Car/index',{
-                        cars: cars,
-                        pagination: {
-                            page: 1,
-                            href:'/admin/cars/',
-                            count: Math.round((count / sails.config.personnalConfig.pagination.cars.admin.limit))
-                        }
-                    });
-                });
-            });
-        }
-    },
+  index: function (req, res) {
+    res.locals.layout = 'Admin/layout';
 
-    edit: function (req, res) {
-      res.locals.layout = 'Admin/layout';
-        console.log('Method: '+req.method);
-        console.log('Body: '+req.body);
-        if (req.method === "POST") {
+    Car.count(function (err, count) {
+      Car.find().sort('name ASC').paginate({
+        page: req.param('page') || 1,
+        limit: sails.config.personnalConfig.pagination.cars.admin.limit
+      }).exec(function (err, cars) {
+        return res.view('Admin/Car/index',{
+          cars: cars,
+          pagination: {
+            page: req.param('page') || 1,
+            href:'/admin/cars/',
+            count: Math.round((count / sails.config.personnalConfig.pagination.cars.admin.limit))
+          }
+        });
+      });
+    });
+  },
 
-            dirname = 'images/cars';
-            fileName = req.param('name').split(' ').join('_');
-            reqFile = req.file('thumbnail');
+  edit: function (req, res) {
+    res.locals.layout = 'Admin/layout';
+    console.log('Method: '+req.method);
+    console.log('Body: '+req.body);
 
-            reqFile.upload({
-                dirname: '../public/images/cars',
-                maxBytes: 10000000,
-                saveAs: fileName+'.jpg'
-            },function whenDone(err, uploadedFiles) {
-                if (err)  return res.negotiate(err);
-
-                data = {
-                  name: req.param('name'),
-                  brand: req.param('brand'),
-                  HP: req.param('HP'),
-                  year: req.param('year'),
-                  top_speed: req.param('top_speed')
-                };
-                // If no files were uploaded, respond with an error.
-                if (uploadedFiles.length > 0){
-                    data.thumb = '/'+dirname+'/'+fileName+'.jpg';
-                }
-                //console.log(uploadedFiles);
-                Car.update(req.param('id'), data).exec(function(err, car) {
-                    if (err) {
-                        res.redirect('/admin/cars');
-                    }
-                    Car.find().sort('name ASC').exec(function(error, records) {
-                        return res.view('Admin/Car/index',{
-                            cars: records,
-                            msg: 'Car Updated!'
-                        });
-                    });
-
-                });
-
-
-            });
-        } else {
-            Car.findOne({id: req.param('id')}).exec(function(error, record) {
-                return res.view('Admin/Car/edit',{
-                    car: record
-                });
-            });
-        }
-
+    if (req.method != "POST") {
+      return Car.findOne({id: req.param('id')}).exec(function (error, record) {
+        return res.view('Admin/Car/edit', {car: record});
+      });
     }
+
+    var dirname = 'images/cars';
+    var fileName = req.param('name').split(' ').join('_');
+    var reqFile = req.file('thumbnail');
+
+    reqFile.upload({
+      dirname: '../public/images/cars',
+      maxBytes: 10000000,
+      saveAs: fileName+'.jpg'
+    }, function (err, uploadedFiles) {
+      if (err) return res.negotiate(err);
+
+      var data = {
+        name: req.param('name'),
+        brand: req.param('brand'),
+        HP: req.param('HP'),
+        year: req.param('year'),
+        top_speed: req.param('top_speed')
+      };
+
+      // If no files were uploaded, respond with an error.
+      if (uploadedFiles.length > 0) data.thumb = '/'+dirname+'/'+fileName+'.jpg';
+
+      //console.log(uploadedFiles);
+      Car.update(req.param('id'), data).exec(function(err, car) {
+        if (err) return res.redirect('/admin/cars');
+        Car.find().sort('name ASC').exec(function(error, records) {
+          return res.view('Admin/Car/index',{
+            cars: records,
+            msg: 'Car Updated!'
+          });
+        });
+      });
+    });
+  }
 };
