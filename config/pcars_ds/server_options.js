a8d8fecbe2f682b84ad45a2b97158f7319883b60
tylerbre <tylervanbach@gmail.com>
2015-07-08 22:42:27 -0500

    grabbing server country and region and adding it to pcars server name

--- config/pcars_ds/server_options.js
+++ config/pcars_ds/server_options.js
@@ -2,10 +2,16 @@ var appGlobals = require('../globals.js').globals;
 var _ = require('lodash');
 
 module.exports = {
-  name: _.camelCase(appGlobals.sitename) + '-' + Math.round(Math.random() * 10000),
+  name: _name(_name(_.camelCase(appGlobals.sitename))),
   password: '',
   maxPlayerCount: 64,
   bindIP: '', // your home ip
   httpApiInterface: '127.0.0.1',
-  httpApiPort: 9000
+  httpApiPort: 9000,
+  _name: _name,
+  _siteName: _.camelCase(appGlobals.sitename)
+}
+
+function _name (prefix) {
+  return prefix + '-' + Math.round(Math.random() * 10000);
 }
