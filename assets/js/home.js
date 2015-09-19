7634ba659c7a79455f76d5ea04fd693aac4a1de1
surtic86 <surtic86@gmail.com>
2015-09-19 22:00:01 +0200

    fix home.js attributes undefined

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,10 +1,14 @@
-(function($){
+(function($) {
 
-	io.socket.on('homeData', function(e){
-		for (var i in e.players) {
-			$('#home_server--connected').append(e.Connected[i].name + ' | '+ e.Connected[i].LoadState);
-		}
-		$('#home_server--status').html(e.Status.response.attributes.SessionState);
-	});
+  io.socket.on('homeData', function(e) {
+    for (var i in e.players) {
+      $('#home_server--connected').append(e.Connected[i].name + ' | ' + e.Connected[i].LoadState);
+    }
+
+    if (typeof e.Status.response != 'undefined') {
+      $('#home_server--status').html(e.Status.response.attributes.SessionState);
+    }
+
+  });
 
 })(jQuery);
