e6376890dd82a6dfb21805526136b81f7788e919
surtic86 <surtic86@gmail.com>
2015-09-19 13:14:10 +0200

    Policies CodeStyling / JSLint fixes

--- api/policies/isAdmin.js
+++ api/policies/isAdmin.js
@@ -1,7 +1,7 @@
-module.exports = function (req, res, next) {
+module.exports = function(req, res, next) {
   if (req.session.user && req.session.user.admin) return next();
 
   if (req.wantsJSON) return res.send(401);
 
   res.redirect('/login');
-}
+};
--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -9,18 +9,18 @@
  */
 module.exports = function(req, res, next) {
 
-    // If `req.session.me` exists, that means the user is logged in.
-    if (req.session.me) return next();
+  // If `req.session.me` exists, that means the user is logged in.
+  if (req.session.me) return next();
 
-    // If this is not an HTML-wanting browser, e.g. AJAX/sockets/cURL/etc.,
-    // send a 401 response letting the user agent know they need to login to
-    // access this endpoint.
-    if (req.wantsJSON) {
-        return res.send(401);
-    }
-    sails.sockets.blast({
-        msg: user.username+' Connected'
-    });
-    // Otherwise if this is an HTML-wanting browser, do a redirect.
-    return res.redirect('/login');
+  // If this is not an HTML-wanting browser, e.g. AJAX/sockets/cURL/etc.,
+  // send a 401 response letting the user agent know they need to login to
+  // access this endpoint.
+  if (req.wantsJSON) {
+    return res.send(401);
+  }
+  sails.sockets.blast({
+    msg: user.username + ' Connected'
+  });
+  // Otherwise if this is an HTML-wanting browser, do a redirect.
+  return res.redirect('/login');
 };
